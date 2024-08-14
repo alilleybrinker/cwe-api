ebd71b0a4ef6c6997bf5838d4715022338e9362f
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-08-14 10:22:56 -0700

    chore: Added crate-level documentation
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! `cwe-api` is a crate for interacting with the [Common Weakness Enumeration
+//! (CWE) REST API][cwe_api]. The code in this crate is generated with
+//! [Progenitor][progenitor] from the official [CWE OpenAPI JSON
+//! specification][cwe_openapi].
+//!
+//! [cwe_api]: https://github.com/CWE-CAPEC/REST-API-wg/blob/main/Quick%20Start.md
+//! [cwe_openapi]: https://github.com/CWE-CAPEC/REST-API-wg/blob/main/openapi.json
+//! [progenitor]: https://github.com/oxidecomputer/progenitor
+
 use progenitor::generate_api;
 
 generate_api!("api/cwe-api.json");
